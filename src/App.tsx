c45063f59399ab336c62a7f5a81649603d50c8a5
leela67 <Leela.Karthik@cryptoforce.in>
2025-10-25 00:44:23 +0530

    nre build

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Rivaaj-Couture">
+      <BrowserRouter basename="/fashion-frontiers-page">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/Rivaaj-Couture/",
+   base: "/fashion-frontiers-page/",
   build: {
     outDir: "docs",
   },
